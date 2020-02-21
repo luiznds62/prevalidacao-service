bf6ccd31de04c409e867be9402b0fc30d15b52cd
luiz.santos <luiz.santos@betha.com.br>
2020-02-21 09:50:09 -0300

    .

--- dist/config/environments.js
+++ dist/config/environments.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.environments = {
-    server: { port: process.env.SERVER_PORT || 3000 }
+    server: { port: process.env.PORT || 3000 }
 };
 //# sourceMappingURL=environments.js.map
